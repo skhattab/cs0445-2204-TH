c4045a4d83d5b76de924bca482961b49b8370bda
skhattab <skhattab@cs.pitt.edu>
2020-01-23 14:36:19 -0500

    Lecture 6 - 1/23/2020

--- Bag/LinkedBag.java
+++ Bag/LinkedBag.java
@@ -1,12 +1,22 @@
 public class LinkedBag<T> implements BagInterface<T>{
 
+  private Node firstNode;
+  private int size;
+
+  public LinkedBag(){
+    firstNode = null;
+    size=0;
+  }
 
   /** Adds an item to the bag if possible
     * @param item the item to be added
     * @return true if adding was sucessful, or false otherwise
     */
   public boolean add(T item){
-    return false;
+    boolean result = true;
+    firstNode = new Node(item, firstNode);
+    size++;
+    return result;
   }
 
   /** Removes an aribtrary instance of a given item from the bag.
@@ -14,22 +24,42 @@ public class LinkedBag<T> implements BagInterface<T>{
     * @return the removed item, or null if item is not in the bag
     */
   public T remove(T item){
-    return null;
+    T result = null;
+    if(!isEmpty()){
+      //go to the node containing item
+      Node toRemove = referenceTo(item);
+      if(toRemove != null){
+        //save the value into result
+        result = toRemove.data;
+        //replace by firstNode's data
+        toRemove.data = firstNode.data;
+        //remove first node
+        firstNode = firstNode.next;
+        size--;
+      }
+    }
+    return result;
   }
 
   /** Removes an arbitrary item from the Bag
     * @return the removed item, or null if the bag is empty
     */
   public T remove(){
-    return null;
+    T result = null;
+    if(!isEmpty()){
+      result = firstNode.data;
+      firstNode = firstNode.next;
+      size--;
+    }
+    return result;
   }
 
   public int size(){
-    return 0;
+    return size;
   }
 
   public boolean isEmpty(){
-    return false;
+    return size == 0;
   }
 
   public boolean isFull(){
@@ -37,19 +67,40 @@ public class LinkedBag<T> implements BagInterface<T>{
   }
 
   public int getFrequencyOf(T item){
-    return 0;
+    int result = 0;
+    Node current = firstNode;
+    while(current != null){
+        if(current.data.equals(item)){
+          result++;
+        }
+        current = current.next;
+    }
+    /* while(firstNode != null){
+      firstNode = firstNode.next;
+    } */ // <= DON'T DO THAT
+    return result;
   }
 
   public boolean contains(T item){
-    return false;
+    return referenceTo(item) != null ;
   }
 
   public void clear(){
-
+    firstNode = null;
+    size=0;
   }
 
   public T[] toArray(){
-    return null;
+    @SuppressWarnings("unchecked")
+    T[] result = (T[])new Object[size];
+    int index = 0;
+    Node current = firstNode;
+    while(current != null){
+      result[index] = current.data;
+      current = current.next;
+      index++;
+    }
+    return result;
   }
 
   public BagInterface<T> intersection(BagInterface<T> anotherBag){
@@ -62,6 +113,34 @@ public class LinkedBag<T> implements BagInterface<T>{
 
   public BagInterface<T> difference(BagInterface<T> another){
     return null;
+  }
+
+  private Node referenceTo(T item){
+    Node result = null;
+    Node current = firstNode;
+    while(current != null){
+      if(current.data.equals(item)){
+        result = current;
+        break;
+      }
+      current = current.next;
+    }
+    return result;
+
+  }
+
+  //nested class
+  private class Node {
+    T data;
+    Node next;
+
+    private Node(T data){
+      this(data, null);
+    }
+    private Node(T data, Node next){
+      this.data = data;
+      this.next = next;
+    }
   }
 
 }
